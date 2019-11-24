522d7f3f9b0efea8e1016c306cb02ed6b50121cb
Tyler Stewart <tyler.stewart1026@gmail.com>
2019-11-24 16:38:24 -0700

    added API routes at a basic level for trails table, created Trails variable

--- models/skinder.js
+++ models/skinder.js
@@ -1,7 +1,114 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
-  return Example;
+  // var Example = sequelize.define("Example", {
+  //   text: DataTypes.STRING,
+  //   description: DataTypes.TEXT
+  // });
+  // return Example;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Define Trails Variable for API routes.
+var Trails = sequelize.define("Trails", {
+  trailId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      len: [1]
+    }
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    validate: {
+      len: [1, 100]
+    }
+  },
+  difficulty: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    validate: {
+      len: [1]
+    }
+  },
+  latLong: {
+    type: DataTypes.DECIMAL,
+    allowNull: true
+  },
+  length: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    validate: {
+      len: [1, 100]
+    }
+  },
+  rating: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    validate: {
+      len: [1, 100]
+    }
+  }
+});
+return Trails;
 };
+
+
+
+
+
+
+
+
+
+
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,94 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // app.get("/api/examples", function (req, res) {
+  //   db.Example.findAll({}).then(function (dbExamples) {
+  //     res.json(dbExamples);
+  //   });
+  // });
+
+  // // Create a new example
+  // app.post("/api/examples", function (req, res) {
+  //   db.Example.create(req.body).then(function (dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
+
+  // // Delete an example by id
+  // app.delete("/api/examples/:id", function (req, res) {
+  //   db.Example.destroy({
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   }).then(function (dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Routes Trails API
+  app.get("/api/trails", function (req, res) {
+    db.trails.findAll({}).then(function (trailsResults) {
+      res.json(trailsResults);
     });
   });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.get("/api/trails", function (req, res) {
+    db.trails.findOne({
+      where: {
+        trailsId: req.body.trailId
+      }
+    }).then(function (trailsResults) {
+      res.json(trailsResults);
     });
   });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  app.get("/api/trails", function (req, res) {
+    db.trails.findOne({
+      where: {
+        rating: req.body.rating
+      }
+    }).then(function (trailsResults) {
+      res.json(trailsResults);
+    });
+  });
+  app.get("/api/trails", function (req, res) {
+    db.trails.findOne({
+      where: {
+        length: req.body.trailLength
+      }
+    }).then(function (trailsResults) {
+      res.json(trailsResults);
     });
   });
 };
