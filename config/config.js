fc195f12e63990b85d30e5369526beb5410bb99e
Tyler Stewart <tyler.stewart1026@gmail.com>
2019-11-25 21:15:10 -0700

    working routes, added all user and forum routes, added new keys, created basic AJAX call

---
+++ config/config.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+let config = {
+    "development": {
+      "username": "root",
+      "password": process.env.DB_PASSWORD,
+      "database": "skinder_db",
+      "host": "127.0.0.1",
+      "port": 3306,
+      "dialect": "mysql",
+      "operatorsAliases": false
+    }
+  }
+  module.exports = config;
