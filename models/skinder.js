74584e2cd18f24987a93f00284f3b14eab416c9f
andrew stehno <andrew_stehno@yahoo.com>
2019-11-26 11:07:33 -0700

    syncing

--- models/skinder.js
+++ models/skinder.js
@@ -1,7 +1,66 @@
-module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
-  return Example;
+module.exports = function (sequelize, DataTypes) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  let Forum = sequelize.define("Forum", {
+    title: {
+      type: DataTypes.STRING,
+      notNull: true,
+      validate: {
+        len: [1, 100],
+        notEmpty: true
+      }
+    },
+    author: {
+      type: DataTypes.STRING,
+      notNull: true,
+      validate: {
+        len: [1, 50],
+        notEmpty: true
+      }
+    },
+    // category: {
+    //   type: DataTypes.STRING,
+    //   notNull: true,
+    //   validate: {
+    //     len: [1, 50],
+    //   },
+    //   defaultValue: "Skiing"
+    // },
+    body: {
+      type: DataTypes.STRING,
+      notNull: true,
+      validate: {
+        len: [1, 500],
+        notEmpty: true
+      }
+    }
+  })
+  return Forum;
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,115 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // Get all 
+  app.get("/api/forums", function(req, res) {
+    db.Forum.findAll({}).then(function(dbForum) {
+      res.json(dbForum);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // Get route for returning posts of a specific category
+  app.get("/api/forums/category/:category", function(req, res) {
+    db.Forum.findAll({
+      where: {
+        category: req.params.category
+      }
+    }).then(function(dbPost) {
+        res.json(dbPost);
+      });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // Get route for retrieving a single post
+  app.get("/api/forums/:id", function(req, res) {
+    db.Forum.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbForum) {
+        res.json(dbForum);
+      });
+  });
+
+  // POST route for saving a new post
+  app.post("/api/forums", function(req, res) {
+    console.log(req.body);
+    db.Forum.create({
+      title: req.body.title,
+      author: req.body.author,
+      category: req.body.category,
+      body: req.body.body
+    }).then(function(dbForum) {
+        res.json(dbForum);
+      });
+  });
+
+  // DELETE route for deleting posts
+  app.delete("/api/forums/:id", function(req, res) {
+    db.Forum.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbForum) {
+        res.json(dbForum);
+      });
+  });
+
+  // PUT route for updating posts
+  app.put("/api/forums", function(req, res) {
+    db.Forum.update(req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbForum) {
+        res.json(dbForum);
+      });
   });
 };
