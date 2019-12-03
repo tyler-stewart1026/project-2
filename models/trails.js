4100120c2ac1c60a2a32f459f0f616df42b61aa1
andrew stehno <andrew_stehno@yahoo.com>
2019-12-03 11:52:30 -0700

    created all html routes

--- models/trails.js
+++ models/trails.js
@@ -1,46 +1,45 @@
-module.exports = function (sequelize, DataTypes) {
-    // Define Trails Variable for API routes.
-    var Trails = sequelize.define("Trails", {
-      trailId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        validate: {
-          len: [1, 100]
-        }
-      },
-      difficulty: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        validate: {
-          len: [1]
-        }
-      },
-      latLong: {
-        type: DataTypes.DECIMAL,
-        allowNull: true
-      },
-      length: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        validate: {
-          len: [1, 100]
-        }
-      },
-      rating: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        validate: {
-          len: [1, 100]
-        }
+module.exports = function(sequelize, DataTypes) {
+  // Define Trails Variable for API routes.
+  var Trails = sequelize.define("Trails", {
+    trailId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
       }
-    });
-    return Trails;
-  };
-  
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [1, 100]
+      }
+    },
+    difficulty: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        len: [1]
+      }
+    },
+    latLong: {
+      type: DataTypes.DECIMAL,
+      allowNull: true
+    },
+    length: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        len: [1, 100]
+      }
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        len: [1, 100]
+      }
+    }
+  });
+  return Trails;
+};
