a42c67156a02d5195b0a303fe95e00844efd38e5
Tyler Stewart <tyler.stewart1026@gmail.com>
2019-12-08 22:24:55 -0700

    fixing deployment issues with user.js

--- public/js/user.js
+++ public/js/user.js
@@ -1,40 +1,41 @@
-$(document).ready(function() {
-// newUser = {};
-// scores = [];
+$(document).ready(function () {
+  // newUser = {};
+  // scores = [];
 
-$("#submit-user").on("click", function(event) {
-  event.preventDefault();
-  
+  $("#submit-user").on("click", function (event) {
+    event.preventDefault();
+
     var newUser = {
-        name: $("#name").val().trim(),
-        email: $("#email").val().trim(),
-        phone: $("#phone").val().trim(),
-        zipcode: $("#zipcode").val().trim(),
-        type: $("#q1").val().trim(),
-        ability: $("#q2").val(),
-        powderhound: $("#q3").val()
+      name: $("#name").val().trim(),
+      email: $("#email").val().trim(),
+      phone: $("#phone").val().trim(),
+      zipcode: $("#zipcode").val().trim(),
+      type: $("#q1").val().trim(),
+      ability: $("#q2").val(),
+      powderhound: $("#q3").val()
     };
 
-  //console.log('newUser', newUser);
+    //console.log('newUser', newUser);
     var scores = [
-        //$("#q1").val(),
-        $("#q2").val(),
-        $("#q3").val()
-        ];
+      //$("#q1").val(),
+      $("#q2").val(),
+      $("#q3").val()
+    ];
     console.log('scores', scores)
-  
+
     $.ajax("/api/user", {
       type: "POST",
       data: newUser,
-      success: function(result){
+      success: function (result) {
         //console.log(result);
-        return result }
-    }).then(function(response) {
+        return result
+      }
+    }).then(function (response) {
       //console.log("newUser", newUser);
       var id = response.id;
 
       //console.log('response', response.id);
-      
+
       console.log("new user created");
       window.location.href = "/user/" + id;
       res.json(res);
@@ -43,64 +44,65 @@ $("#submit-user").on("click", function(event) {
 
   });
 
-  $("#findMatch").on("click", function(event) {
+  $("#findMatch").on("click", function (event) {
     event.preventDefault();
     $.ajax("/api/user", {
       type: "GET",
-      success: function(result){
+      success: function (result) {
         //console.log(result);
-        return result }
-    }).then(function(response) {
+        return result
+      }
+    }).then(function (response) {
       //console.log("response", response[0]);
-    
-    //placeholder for pulling my user info
+
+      //placeholder for pulling my user info
       var myUser = {
-      name: "Matt",
-      email: "matt@sbsef.com",
-      phone: "801-541-1587",
-      zipcode: "84117",
-      type: "Skier",
-      ability: "5" ,
-      powderhound: "5"
-  };
+        name: "Matt",
+        email: "matt@sbsef.com",
+        phone: "801-541-1587",
+        zipcode: "84117",
+        type: "Skier",
+        ability: "5",
+        powderhound: "5"
+      };
 
-  //placeholder for bestMatch
-  //console.log("response", response[14].ability);
-    //var user = JSON.stringify(localStorage.setItem('user', {{{user}}}));
-    console.log("user", myUser);
-    
-    var myAbility = parseInt(myUser.ability);
-    var myPowderhound = parseInt(myUser.powderhound);
-    var scores = [
-      myAbility, myPowderhound
-    ];
+      //placeholder for bestMatch
+      //console.log("response", response[14].ability);
+      //var user = JSON.stringify(localStorage.setItem('user', {{{user}}}));
+      console.log("user", myUser);
 
-    console.log("scores", scores);
-    
-     // console.log("response", response);
+      var myAbility = parseInt(myUser.ability);
+      var myPowderhound = parseInt(myUser.powderhound);
+      var scores = [
+        myAbility, myPowderhound
+      ];
+
+      console.log("scores", scores);
+
+      // console.log("response", response);
       //setting variables to find match
       var myFriendScores = req.body.scores;
       var scoresArr = [];
       var bestMatch = 0;
       console.log("response.length", response.length);
       //loop through friends
-      for(var i = 0; i < response.length; i++){
+      for (var i = 0; i < response.length; i++) {
         var scoresDiff = 0;
         console.log("response[i]", response);
         //loop through friends scores
-        for(var j = 0; j < myFriendScores.length; j++){
+        for (var j = 0; j < myFriendScores.length; j++) {
           //compare each friends scores against the new friend
           scoresDiff += (Math.abs(parseInt(response[i].scores[j]) - parseInt(myFriendScores[j])));
         }
-        
+
         scoresArr.push(scoresDiff);
-        
+
       }
       console.log("response", response);
-      
+
       //loop through scores to find best match in array
-      for(var i = 0; i < scoresArr.length; i++){
-        if(scoresArr[i] <= scoresArr[bestMatch]){
+      for (var i = 0; i < scoresArr.length; i++) {
+        if (scoresArr[i] <= scoresArr[bestMatch]) {
           bestMatch = i;
         }
       }
@@ -109,13 +111,10 @@ $("#submit-user").on("click", function(event) {
       //return bestMatch data
       var yourBestFriend = response[bestMatch];
       res.json(yourBestFriend);
-      
+
       //push new friend object to list of friends array
       response.push(req.body);
+    });
+
+  })
 });
-
-});
-
-});
-
-
