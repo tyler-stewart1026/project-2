d347579c39569accf457885db735022f3aaec821
Tyler Stewart <tyler.stewart1026@gmail.com>
2019-12-09 18:10:01 -0700

    fixing stuff with deployment

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -40,22 +40,22 @@ module.exports = function(app) {
     res.render("create-user");
   });
 
-  app.get("/", function(req, res) {
-    db.Trails.findAll({}).then(function() {
-      var hbsObject = {
-        trailName: "Tyler trail",
-        trailLength: "0.5",
-        trailDiff: "Black Diamond",
-        trailLocation: "Aspen, Colorado",
-        rating: "3",
-        trailPic:
-          "https://cdn.pixabay.com/photo/2017/02/14/03/03/ama-dablam-2064522_960_720.jpg",
-        trailDesc:
-          "Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters."
-      };
-      res.render("index", hbsObject);
-    });
-  });
+  // app.get("/", function(req, res) {
+  //   db.Trails.findAll({}).then(function() {
+  //     var hbsObject = {
+  //       trailName: "Tyler trail",
+  //       trailLength: "0.5",
+  //       trailDiff: "Black Diamond",
+  //       trailLocation: "Aspen, Colorado",
+  //       rating: "3",
+  //       trailPic:
+  //         "https://cdn.pixabay.com/photo/2017/02/14/03/03/ama-dablam-2064522_960_720.jpg",
+  //       trailDesc:
+  //         "Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters."
+  //     };
+  //     res.render("index", hbsObject);
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function (req, res) {
